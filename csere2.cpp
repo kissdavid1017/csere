ea587ae1a98c5c106febbc4df8ea1e8d5433ee7e
kissdavid1017 <55405306+kissdavid1017@users.noreply.github.com>
2019-09-19 14:01:27 +0300

    Add files via upload

---
+++ csere2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<< "a= ";cin>>a; cout<<endl;
+ cout<< "b= ";cin>>b; cout<<endl;
+
+ a=a*b;
+ b=a/b;
+ a=a/b;
+
+ cout<< "uj a= "<<a<<endl;
+ cout<< "uj b= "<<b<<endl;
+}
